c4269e8905f6521f83d62b2937eaf09aa3c195d7
Jan Krupiński <jk.krupinski@gmail.com>
2022-03-22 12:45:32 +0100

    Get options from input

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 levels = {
-    "beginner": {"chances": 8},
-    "intermediate": {"chances": 5},
-    "advanced": {"chances": 3}
+  "beginner": {"chances": 8},
+  "intermediate": {"chances": 5},
+  "advanced": {"chances": 3}
 }
 
 languages = {
-    "english": "en",
-    "german": "de",
-    "italian": "it",
-    "spanish": "es"
+  "english": "en",
+  "german": "de",
+  "italian": "it",
+  "spanish": "es"
 }
 
 mode = {
-    "single": 1,
-    "multiplayer": 2
+  "single": 1,
+  "multiplayer": 2
 }
 
 
 class Game:
-    def __init__(self, players_count):
-        self.players_count = players_count
+  def __init__(self, players_count):
+    self.players_count = players_count
 
-    def __play(self):
-        print("Game started")
+  def __play(self):
+    print("Game started")
 
 
 class Hangman(Game):
-    def __init__(self, players_count, level):
-        super().__init__(players_count)
-        self.level = level
+  def __init__(self, players_count, level, language):
+    super().__init__(players_count)
+    self.level = level
+    self.language = language
+
+print("HANGMAN")
 
 
-hangman = Hangman(mode["single"], levels["beginner"])
+def selectOption(options_dict, options_name):
+  option = ""
+  while option not in options_dict:
+    option = input(f"Select {options_name} ({', '.join(list(options_dict.keys()))}): ").lower()
+
+  print(f"Selected {option}")
+  return option;
+
+
+mode_input = selectOption(mode, "mode")
+levels_input = selectOption(levels, "level")
+languages_input = selectOption(languages, "language")
+
+hangman = Hangman(mode[mode_input], levels[levels_input], languages[languages_input])
